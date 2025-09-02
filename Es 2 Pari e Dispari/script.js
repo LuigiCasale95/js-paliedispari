58dec63f578bf7249de4acf59467091420fbce8c
LuigiCasale95 <luigicasale95@gmail.com>
2025-09-02 15:59:53 +0200

    Completamento es. pari o dispari

--- Es 2 Pari e Dispari/script.js
+++ Es 2 Pari e Dispari/script.js
@@ -5,3 +5,49 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto. */
 
+
+//Numero estratto dall'utente
+let numeroUtente = parseInt (prompt("Scegli un numero da 1 a 5"))
+console.log(`Il numero scelto dall'utente è **${numeroUtente}**`);
+
+/* Scelta utente tra pari e dispari */
+let sceltaUtentePariDispari = prompt("Sceri se pari o dispari")
+console.log(`L'utente ha scelto ${sceltaUtentePariDispari}`);
+
+//Numero random estratto dal PC
+ let NumeroPc = randomPcNum()
+ console.log(`Il numero scelto dal pc è ${NumeroPc}`)
+
+ /* Somma dei numeri scelti */
+ let somma = (numeroUtente + NumeroPc)
+ console.log(`Il totale è ${somma}`);
+
+ /* Verifica del numero, se pari o dispari */
+console.log("Ha vinto il " + pariOppureDispari(somma))
+
+ //Elezione del vincitore
+
+
+
+
+
+
+
+
+
+
+ //Funzioni
+function randomPcNum() {
+    let number = Math.floor(Math.random() * 5 ) + 1;
+    console.log(number);
+    
+    return number
+}
+
+function pariOppureDispari(somma) {
+    if (somma % 2 === 0) {
+        return "PARI"
+   } else {
+        return "DISPARI"
+     }
+}
